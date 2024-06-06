9f49b7153e6c6e78766711d9e100c3fdb8183676
Fayexi <ahye.aragoncillo.aa@gmail.com>
2024-06-07 00:52:31 +0800

    add login page

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <form @submit.prevent="login">
+            <input v-model="email" type="email" placeholder="Email">
+            <input v-model="password" type="password" placeholder="Password">
+            <button type="submit">Login</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        };
+    },
+    methods: {
+        login() {
+            this.$store.dispatch('login', {
+                email: this.email,
+                password: this.password
+            });
+        }
+    }
+};
+</script>
